5f2cae0d0e22d6315029b6a93d0d4e656e098c3e
Байрамова Арзу Ариф <Bayra_zk57@itstep.edu.az>
2024-11-30 15:40:39 +0400

    update endpoint

--- PythonProject3/app.py
+++ PythonProject3/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from services.film_services import get_html_url,compare_descriptions
+from services.film_services import get_html_url,compare_descriptions,get_film_stream,get_serial_stream
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -25,14 +25,26 @@ def generate_movie_url():
         if similarity < 0.5:
             return jsonify({"error": "Description does not match"}), 400
 
+        if rezka_result['type'] == 'Movie':
+            stream_result = get_film_stream(movie_title)
+        elif rezka_result['type'] == 'Serial':
+            stream_result = get_serial_stream(movie_title)
+        else:
+            return jsonify({"error": "Unknown type returned from Rezka"}), 400
+
+        if stream_result.get('status') == 'error':
+            return jsonify({"error": stream_result.get('message')}), 400
+
         return jsonify({
-            "url": rezka_result['url'],
             "type": rezka_result['type'],
-            "similarity": similarity
+            "url": rezka_result['url'],
+            "similarity": similarity,
+            "streams": stream_result
         }), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
